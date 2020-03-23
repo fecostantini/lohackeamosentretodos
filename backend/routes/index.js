4811255c21cb7a422a07404ee9a309ba522ac591
Panchi <franciscostantini@gmail.com>
2020-03-23 17:43:08 -0300

    feat(backend): peticion para obtener las últimas noticias de la OMS

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,5 +2,6 @@ const express = require('express');
 const router = express.Router();
 
 router.use('/twitter', require('./rutasTwitter'));
+router.use('/webscrapping', require('./rutasWebScrapping'));
 
 module.exports = router;
